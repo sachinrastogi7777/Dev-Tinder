0346fcbcc028c2adb30326560b0d67a29781f3ab
Sachin Rastogi(sachin.rastogi) <sachin.rastogi@infosys.com>
2025-08-28 09:13:59 +0530

    Added some get apis.

--- app.js
+++ app.js
@@ -3,29 +3,49 @@ const connectDb = require("./config/database");
 const User = require("./models/user");
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
     try {
-        const user = new User({
-            firstName: "Sachin",
-            lastName: "Rastogi",
-            email: "sachin@gmail.com",
-            password: "Sachin@12345",
-            dateOfBirth: new Date("1997-10-23"),
-            age: 27,
-            gender: "Male",
-            mobileNumber: 8709254547
-        });
+        const user = new User(req.body);
         await user.save();
         res.send("User saved successfully...")
     } catch (error) {
-        console.log("Error saving usr:", error);
+        console.log("Error saving user:", error);
         res.status(500).send("Error saving user: " + error.message);
     }
 })
 
-app.get("/login", (req, res) => {
-    res.send("Logged in...")
-})
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.email;
+    try {
+        const user = await User.find({email: userEmail});
+        if (user.length === 0) {
+            res.status(404).send("User not found!!!");
+        }
+        else {
+            res.send(user)
+        }
+    } catch (error) {
+        console.log("Error finding user:", error);
+        res.status(500).send("Error finding user: " + error.message);
+    }
+});
+
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        if (!users) {
+            res.status(404).send("Users not found!!!");
+        }
+        else {
+            res.send(users);
+        }
+    } catch (error) {
+        console.log("Error finding user:", error);
+        res.status(500).send("Error finding user: " + error.message);
+    }
+});
 
 connectDb().then(() => {
     console.log("Database connected successfully...");
